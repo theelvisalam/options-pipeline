dbe0ce3e6c4adb19ef00ae5eec73b163f88ba611
Elvis Alam <alamelvism@gmail.com>
2026-06-22 23:36:11 -0500

    ticker data, expirations, and options chain

--- websocket.py
+++ websocket.py
@@ -1,25 +1,46 @@
 import yfinance as yf
 import asyncio
 
-# qqq = yf.Ticker('QQQ')
-
-# info = qqq.info
-# for k, v in info.items():
-    # print(k,v)
-
 def message_handler(message):
     print('Recieved Message:', message)
 
-
 async def main():
+    tk_list = [
+        'QQQ', 
+        'SPY'
+    ]
+    tk_data = {}
+    tk_exp = {}
+    tk_chain = []
 
+#   get ticker data 
+    for tk in tk_list:
+        tk_data[tk] = yf.Ticker(tk)
+    print(tk_data)
+    print("--------")
+#   get options expiration dates 
+    for tk, data in tk_data.items():
+        tk_exp[tk] = data.options
+    print(tk_exp)
+    print("--------")
+#   get options chains
+    for tk in tk_list:
+        for x in tk_exp[tk]:
+            tk_chain.append(tk_data[tk].option_chain(x))
+    print(tk_chain)
+    print("--------")
+
+    # def options_expirations(ticker_list):
+        # for t in ticker_list:
+            # expirations = t.options
+        # return expirations
+    # options_expirations(tickers)
+    
     async with yf.AsyncWebSocket() as ws:
         await ws.subscribe(
-            ['QQQ',
-             'SPY']
+            tk_list
         )
-        await ws.listen()
-
+        await ws.listen(message_handler)
+        
 asyncio.run(main())
-# {'id': 'QQQ', 'price': 735.975, 'time': '1781797426000', 'exchange': 'NGM', 'quote_type': 20, 'market_hours': 1, 'change_percent': 1.8636373, 'day_volume': '18299202', 'change': 13.464966, 'price_hint': '2'}
 
