0fe1bc807a4cfee0b92da798c30679adc99bdfb9
Elvis Alam <alamelvism@gmail.com>
2026-06-25 14:01:22 -0500

    additions to kafka producer

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,6 +1,6 @@
 from confluent_kafka import Producer
-
-p = Producer({'bootstrap.servers': 'broker1, broker2'})
+import json
+p = Producer({'bootstrap.servers': 'localhost:9092'})
 
 def delivery_report():
     if err is not None:
@@ -11,9 +11,40 @@ def delivery_report():
 def publish_chain(tk_chain):
     for chain in tk_chain:
         for _, row in chain.calls.iterrows():
+            '''
+            ROWS IN chain.calls
+            contractSymbol
+            lastTradeDate
+            strike
+            lastPrice
+            bid
+            ask
+            change
+            percentChange
+            volume
+            openInterest
+            impliedVolatility
+            inTheMoney
+            contractSize
+            currency
+            '''
             message = {
+                'type': 'call',
+                'expiration': str(row.name),
+                'strike': row['strike'],
+                'bid': row['bid'],
+                'ask': row['ask'],
+                'impliedVolatility': row['impliedVolatility'],
+                'volume': row['volume']
+            }
+            p.produce(
+                'publish-chain',
+                key = str(row['strike']),
+                value = json.dumps(message),
+                callback = delivery_report
 
-            }   
+            )
+            p.flush()
 
 def publish_price_tick(ticker, price):
     message = {
@@ -23,8 +54,8 @@ def publish_price_tick(ticker, price):
     p.produce(
         'price-ticks',
         key = ticker,
-        value = json.dumps(message)
+        value = json.dumps(message),
         callback = delivery_report
     )
-    producer.flush()
+    p.flush()
 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kafka_producer import publish_chain 
 from tickers import get_tickers, get_expirations, get_chains
 from websocket import start_stream
 import yfinance as yf
@@ -8,5 +9,7 @@ tk_list = ['QQQ', 'SPY', 'SNDK']
 tk_data = get_tickers(tk_list)
 tk_exp = get_expirations(tk_data)
 tk_chain = get_chains(tk_list, tk_data, tk_exp)
+
+publish_chain(tk_chain)
 
 asyncio.run(start_stream(tk_list)) 
