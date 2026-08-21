8f4c388565dbe7492a5e0aee1046396d024eb2c3
Elvis Alam <alamelvism@gmail.com>
2026-08-20 23:27:20 -0500

    fetch test

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
+    allow_origins = ["http://localhost:5173"],
     allow_methods = ["GET"],
     allow_headers = ["*"]
 )
